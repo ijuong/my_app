9dc39e0a7c114366511b199730632da346cb0384
ijuong <ijuong@naver.com>
2017-03-23 10:49:05 +0900

    study

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello world!');
-});
+// app.get('/', function(req, res){
+//   res.send('Hello world!');
+// });
 
+// app.use(express.static(__dirname + '/public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+console.log(__dirname);
 app.listen(3000, function(){
   console.log('Server on!');
 });
